6524888beed9ae999f4a7dbc7cd1a55ead265b02
Abdul Azeez <151101928+AbdulAzeez1312005@users.noreply.github.com>
2025-09-09 00:20:49 +0530

    Update companyRoutes.js

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -3,6 +3,10 @@ import { getCompanies, createCompany } from "../controllers/companyController.js
 
 const router = express.Router();
 
+// Test route
+router.get("/test", (req, res) => {
+  res.send("✅ Companies route works");
+});
 // Routes
 router.get("/", getCompanies);
 router.post("/", createCompany);
